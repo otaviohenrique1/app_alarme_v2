0f9cb403f539e32c8fc1c2fc379675cbef2fbd99
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-11-04 23:02:11 -0300

    commit 6

--- src/components/ItemLista.tsx
+++ src/components/ItemLista.tsx
@@ -1,15 +1,21 @@
-import { List, Switch } from 'react-native-paper';
+import { View } from "react-native";
+import { IconButton, List, MD3Colors, Switch, Modal, Portal, Text } from 'react-native-paper';
 import { AlarmeDatabase, useAlarmeDatabase } from "../database/useAlarmeDatabase";
 import { useEffect, useState } from 'react';
 
-interface ItemListaProps  {
+interface ItemListaProps {
   data: AlarmeDatabase
 }
 
 export function ItemLista(props: ItemListaProps) {
   const { id, tempo, nome, ativo } = props.data;
   const [isSwitchOn, setIsSwitchOn] = useState(false);
+  const [visible, setVisible] = useState(false);
   const alarmeDatabase = useAlarmeDatabase();
+  
+  const showModal = () => setVisible(true);
+  
+  const hideModal = () => setVisible(false);
 
   const onToggleSwitch = async () => {
     setIsSwitchOn(!isSwitchOn)
@@ -20,13 +26,34 @@ export function ItemLista(props: ItemListaProps) {
     setIsSwitchOn(ativo as boolean);
   }, []);
 
+  async function remover(id: number) {
+    try {
+      await alarmeDatabase.remover(id);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <List.Item
       title={tempo}
       description={nome}
       right={props => (
-        <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+          <IconButton
+            icon="delete"
+            iconColor={MD3Colors.tertiary0}
+            size={30}
+            onPress={showModal}
+            // onPress={() => remover(id)}
+          />
+          <Portal>
+            <Modal visible={visible} onDismiss={hideModal} contentContainerStyle={{backgroundColor: 'white', padding: 20}}>
+              <Text>Example Modal.  Click outside this area to dismiss.</Text>
+            </Modal>
+          </Portal>
+        </View>
       )}
       style={{ marginBottom: 10, borderColor: "black", borderWidth: 1, borderRadius: 10 }}
     />
